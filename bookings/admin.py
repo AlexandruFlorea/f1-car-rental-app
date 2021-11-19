a828fda30663a8d4783fa6646e77d8e8a6334c3a
AlexandruFlorea <alexandru.floreaf1@gmail.com>
2021-11-19 16:35:31 +0200

    Admin site customization

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from my_admin.admin import my_admin_site
 from bookings.models import Booking
+from cars.models import Car
+
 
 
 @admin.register(Booking, site=my_admin_site)  # register on the custom admin site
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ['id','date_created', 'paid', 'user', 'car', 'track']
-    search_fields = ['date_created', 'paid', 'user__email', 'car__name', 'track__name']
+    @admin.action(description='Generate Excel report')
+    def generate_report(self, request, queryset):
+        print('Generate report stuff stuff')
 
+    list_display = ['id', 'date_created', 'paid', 'user', 'car', 'track']
+    search_fields = ['date_created', 'paid', 'user__email', 'car__name', 'track__name']
+    list_filter = ['paid', 'date_created', 'car', 'track']
+    actions = [generate_report]
+
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)  # accesses the parent queryset
+
+        if request.user.is_superuser:
+            return queryset
+
+        return queryset.filter(car__owner=request.user)  # can access bookings for owned cars only
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):  # control access to items linked via FK
+        if db_field.name == 'car' and not request.user.is_superuser:
+            kwargs['queryset'] = Car.objects.filter(owner=request.user)  # limit list options to owned cars
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- cars/admin.py
+++ cars/admin.py
@@ -10,7 +10,8 @@ class CarAdmin(admin.ModelAdmin):
     def team_color(obj):
         if obj.color:
             return format_html(
-                f'<div style="width: 20px; height: 20px; border-radius: 50%; background-color: {obj.color}; border: 1px solid black;"></div>'
+                f'<div style="width: 20px; height: 20px; border-radius: 50%; background-color: {obj.color};'
+                f'border: 1px solid black;"></div>'
             )
 
         return 'N/A'
@@ -18,9 +19,33 @@ class CarAdmin(admin.ModelAdmin):
     team_color.short_description = 'team_color'
     team_color.admin_order_field = 'team_color'
 
-    list_display = ['name', 'power_unit', 'races_won', 'handling', 'rate', 'available', 'category', 'team_color']
+    list_display = ['name', 'power_unit', 'races_won', 'handling', 'rate',
+                    'available', 'category', 'team_color', 'owner']
     search_fields = ['name', 'power_unit', 'available', 'category__name', ]
     ordering = ['name', '-rate']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)  # accesses the parent queryset
+
+        if request.user.is_superuser:
+            return queryset
+
+        return queryset.filter(owner=request.user)
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+
+        if not request.user.is_superuser:
+            fields.remove('owner')
+
+        return fields
+
+    # save method overwriting is needed every time we change or add a new object
+    def save_model(self, request, obj, form, change):
+        if not obj.pk and not request.user.is_superuser:
+            obj.owner = request.user  # auto add the owner
+
+        return super().save_model(request, obj, form, change)
 
 
 @admin.register(Category, site=my_admin_site)  # register on the custom admin site
